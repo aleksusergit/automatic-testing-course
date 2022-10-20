b364fae695d25652f2fa3a647f8b858abec30618
aleksandr cherkasin <aleksmail1box@gmail.com>
2022-10-20 17:39:08 +0300

    Add lesson3

---
+++ lesson3/tests/capitalize.test.js
@@ -0,0 +1,6 @@
+import { strict as assert } from 'assert';
+import capitalize from '../src/capitalize.js';
+
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
+assert.strictEqual(capitalize('hello'), 'hello'); // err
