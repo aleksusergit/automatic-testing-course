135f4aa47a02720489eebfe4a86d4e86c5c6e3c4
aleksandr cherkasin <aleksmail1box@gmail.com>
2022-10-20 20:24:51 +0300

    Modified capitalize.test.js (lesson3)

--- lesson3/tests/capitalize.test.js
+++ lesson3/tests/capitalize.test.js
@@ -2,5 +2,8 @@ import { strict as assert } from 'assert';
 import capitalize from '../src/capitalize.js';
 
 assert.strictEqual(capitalize(''), '');
+console.log('You passed 1 test!');
 assert.strictEqual(capitalize('hello'), 'Hello');
+console.log('You passed 2 tests!');
 assert.strictEqual(capitalize('hello'), 'hello'); // err
+console.log('Greeting!\nYou passed all tests!');
